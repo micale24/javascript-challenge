1e1af0d3763637c1ccd77b4847ee6ad29a4ca5e9
micale24 <kaihall.usaf@gmail.com>
2020-12-11 18:52:45 -0500

    started filter button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,23 +12,47 @@ data.forEach((weatherReport) => {
     });
   });
 
- 
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value
-  ul = document.getElementById("list-group");
-  li = ul.getElementsByTagName('li');
+  //Button function to filter search
+  d3.selectAll("button").on("click", function handleChange() {
+    // var inputText = d3.select("#text");
+    var text = d3.select("#text");
+    var userInput = d3.event.target.value.select("#text");
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
+  // var filterDate = userInput;
+      console.log(userInput)
+  //  output.text(filterDate);
+      text.on("change", handleChange);
+    
+  });
+ 
+
+// Function to handle input change
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//   // Declare variables
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById('myInput');
+//   filter = input.value
+//   ul = document.getElementById("list-group");
+//   li = ul.getElementsByTagName('li');
+
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByTagName("a")[0];
+//     txtValue = a.textContent || a.innerText;
+//     if (txtValue.indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//   }
+// }
